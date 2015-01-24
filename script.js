017a833fdaf7eccaefb0003744bc7412e6b165b4
Jimi Vyas <jimipvyas@gmail.com>
2015-01-23 22:20:30 -0500

    make nav bar float on top of page, still not styled (dont look lol)

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var aboveHeight = $('header').outerHeight();
+
+  $(window).scroll(function(){
+      if ($(window).scrollTop() > aboveHeight){
+          $('nav').addClass('fixed').css('top','0').next()
+          .css('padding-top','60px');
+      } else {
+          $('nav').removeClass('fixed').next()
+          .css('padding-top','0');
+      }
+  });
+});
